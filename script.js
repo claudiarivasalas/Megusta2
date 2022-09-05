73b71d028aaac19a327de5702599ed39e54e71e7
claudiarivasalas <claudia.rivas.salas@gmail.com>
2022-09-04 21:05:07 -0400

    Me Gusta2

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+function maslike(){
+    contador = document.querySelector(".bloque3 #gusta1");
+    suma = parseInt(contador.innerHTML);
+    suma = suma +1;
+    contador.innerText = suma + " " + "like(s)";
+    console.log(suma);
+}
+function addlike(){
+    contador = document.querySelector(".bloque3 #gusta2");
+    suma = parseInt(contador.innerHTML);
+    suma = suma +1;
+    contador.innerText = suma + " " + "like(s)";
+    console.log(suma);
+}
+function sumalike(){
+    contador = document.querySelector(".bloque3 #gusta3");
+    suma = parseInt(contador.innerHTML);
+    suma = suma +1;
+    contador.innerText = suma + " " + "like(s)";
+    console.log(suma);
+}
